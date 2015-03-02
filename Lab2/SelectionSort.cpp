24688bc82da6a0fb725f0a4647fb5bf697a1b2e7
Johan Oakes <oakejp12@wfu.edu>
2015-03-01 22:01:14 -0500

    Finish selection sort algorithm

--- Lab2/SelectionSort.cpp
+++ Lab2/SelectionSort.cpp
@@ -1,9 +1,31 @@
 #include "SelectionSort.h"
 
+
 void SelectionSort::sort(string* data, int n) {
 
+	int i, j, min;
 
-    
+	/* Advance position through the entire array */
+	for (j = 0; j < n-1; j++) {
+
+		/* Assume that min the first element */
+		min = j;
+
+		/* Test against elements after j to find the smallest */
+		for (i = j + 1; i < n; i++) {
+
+			/* If less, then element is the new minimum */
+			if (data[i] < data[min])
+				min = i;
+		}
+
+		/* Swap the new smallest with current element */
+		if ( min != j) {
+			int temp = data[j];
+			data[j] = data[min];
+			data[min] = temp;
+		}
+	}   
 }
 
 void SelectionSort::swap(string& s1, string& s2) {
