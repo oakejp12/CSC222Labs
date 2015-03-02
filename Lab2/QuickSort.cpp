cb0d5d494bf5e69d33a1e72cc740f829c7e51cbc
Johan Oakes <oakejp12@wfu.edu>
2015-03-01 23:35:10 -0500

    Add definition and implementation of QuickSort

---
+++ Lab2/QuickSort.cpp
@@ -0,0 +1,49 @@
+#include "QuickSort.h"
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+
+
+void QuickSort::sort(string* data, int n) {
+	quickSort(data, 0, n-1);
+}
+
+void quickSort(string* data, int left, int right) {
+	int index = partition(data, left, right);
+
+	if (left < index - 1) 
+		quickSort(data, left, index - 1); // Sort left half
+
+	if (index < right)
+		quickSort(data, index, right); // Sort right half
+} 
+
+int QuickSort::partition(string* data, int left, int right) {
+
+	// Pick the pivot point
+	string pivot = data[(left+right)/2];
+
+	while (left <= right) {
+		// Find element on the left that should be on the right
+		while(data[left] < pivot) left++;
+
+		// Find element on the right that should be on the left
+		while(data[right] > pivot) right--;
+
+		if (left <= right) {
+			swap(data[left], data[right]);
+			left++;
+			right--;
+		}
+	}
+
+	return left;
+}
+
+
+void SelectionSort::swap(string& s1, string& s2) {
+    string tmp;
+    tmp = s1;
+    s1 = s2;
+    s2 = tmp;
+}
---
+++ Lab2/QuickSort.h
@@ -0,0 +1,24 @@
+/*
+ * Definition of class QuickSort
+ *
+ * author: Johan P. Oakes
+ * date:   3/1/2015
+ */
+
+#ifndef QUICKSORT_H
+#define QUICKSORT_H	
+
+#include "SortAlgorithm.h"
+
+// QuickSort implements the SortAlgorithm interface
+class QuickSort : public SortAlgorithm {
+public:
+    void sort(string*, int);	// implementation of sort function
+private:
+	int partition(string *, int, int);
+	void quickSort(string*, int, int);
+    void swap(string&, string&); // it's own private function, not part of
+				 // the interface
+};
+
+#endif QUICKSORT_H	
