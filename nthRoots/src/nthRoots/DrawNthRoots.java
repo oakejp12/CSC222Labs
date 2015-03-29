cf62379de6d417cb337b495bd3cdaafdb2a21fc5
Johan Oakes <oakejp12@wfu.edu>
2015-03-29 18:42:42 -0400

    Add barebone files for Lab2

---
+++ nthRoots/src/nthRoots/DrawNthRoots.java
@@ -0,0 +1,25 @@
+/*
+ * Author : Johan Oakes
+ * Date: 3/29/15
+ * 
+ * Draw the Nth roots of unity for some give N > 0
+ * Use some kind of data structure to store complex numbers (N x 2 array)
+ * N x 2 array : (real, imaginary)
+ * 
+ */
+
+
+package nthRoots;
+
+import nthRoots.StdDraw;
+
+public class DrawNthRoots {
+	
+	public static void main(String[] args){
+		System.out.println("Saaaan!");
+	}
+	
+	
+	
+
+}
