7c9f06dd7d32fb1022b5f45e27dda7950f8fafd8
Johan Oakes <oakejp12@wfu.edu>
2015-04-01 19:08:19 -0400

    Add output of Fourier matrices

--- nthRoots/src/nthRoots/DrawNthRoots.java
+++ nthRoots/src/nthRoots/DrawNthRoots.java
@@ -20,12 +20,21 @@ import nthRoots.StdDraw;
 public class DrawNthRoots {
 		
 	public static void main(String[] args){
-		
+
+		System.out.println("Starting Lab 3...");
+
 		// Draw the nth roots of unity
-		drawNthRoots();
+		// drawNthRoots();
+
+		// compute x x y matrix F with N
+		// computeFMatrix(8,8,8);
+
+		// System.out.println("");
+
+		// compute F-inverse matrix
+		computeFInverse(8,8,8);
 		
-		
-		return;
+		// System.exit(0);
 	}
 	
 	/* Method to output the Nth roots given N > 0 */
@@ -46,12 +55,63 @@ public class DrawNthRoots {
 				
 				// Create a new complex number based on the real and imaginary parts
 				Complex t = new Complex(x,y);
-					
+
 				// Plot the real and imaginary coordinates
 				StdDraw.point(x,y);
-				
-				System.out.println(t);	
+
+
+				// System.out.println(t);
 			}
 		}
 	}
+
+	/* Computing the Fourier matrix F */
+	public static void computeFMatrix(int i, int j, int N) {
+
+		Complex F[][] = new Complex[i][j];
+
+		for (int m = 0; m < i; m++){
+			for (int n = 0; n < j; n++) {
+
+				double x = Math.cos(2.0 * Math.PI * (m*n)/N);
+				double y = (0.0 - (Math.sin(2.0 * Math.PI * (m*n)/N)));
+
+				Complex t = new Complex(x, y);
+
+				F[m][n] = t;
+				System.out.print(F[m][n] + "    ");
+			}
+			System.out.println("");
+		}
+	}
+
+	/* Compute the Fourier Matrix F - Inverse */
+	public static void computeFInverse(int i, int j, int N) {
+
+		Complex FInverse[][] = new Complex[i][j];
+
+		double oneOverN = 1.0/N;
+		// Create a new complex number from  oneOVerN
+		Complex z = new Complex(oneOverN, 0.0); // (1/N) + 0i
+
+		for (int m = 0; m < i; m++) {
+			for (int n = 0; n < i; n++) {
+
+				double x = Math.cos(2.0 * Math.PI * (m*n)/N);
+				double y = Math.sin(2.0 * Math.PI * (m * n) / N);
+
+				Complex t = new Complex(x,y);
+
+				Complex complete = t.multiply(oneOverN);
+
+				FInverse[m][n] = complete;
+
+				System.out.print(FInverse[m][n] + "    ");
+			}
+			System.out.println("");
+		}
+	}
+
+
+
 }
